7af82eab04955aebba04186bae52c3aa9fbfa34a
Itsuki-m <mizuharu_sawaka14@yahoo.co.jp>
2021-10-27 18:55:14 +0900

    #258でやった別の方法をApp.jsにメモとして追加

--- src/App.js
+++ src/App.js
@@ -51,3 +51,97 @@ function App() {
 }
 
 export default App;
+
+////////////////////////////////
+////////////////////////////////
+////////////////////////////////
+// #258 side-effects & asyncをinComponentsに書く方のやり方(action creatorsに書かないやり方)
+// 別にこれも間違いではない
+// ただcart-actions.jsのsendCartDataにaction creatorsとして書く方がcodeがleanになる
+
+// memo(参考として)
+
+// import { Fragment, useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+
+// import Cart from './components/Cart/Cart';
+// import Layout from './components/Layout/Layout';
+// import Products from './components/Shop/Products';
+// import { uiActions } from './store/ui-slice';
+// import Notification from './components/UI/Notification';
+
+// // to avoid sending data right at the beginning
+// //毎回更新する度にempty dataにoverrideをavoidする
+// let isInitial = true;
+
+// function App() {
+//   const dispatch = useDispatch();
+//   const showCart = useSelector((state) => state.ui.cartIsVisible);
+//   const cart = useSelector((state) => state.cart);
+//   const notification = useSelector((state) => state.ui.notification);
+
+//   useEffect(() => {
+//     const sendCartData = async () => {
+//       dispatch(
+//         uiActions.showNotification({
+//           status: 'pending',
+//           title: 'Sending...',
+//           message: 'Sending cart data!',
+//         })
+//       );
+//       const response = await fetch(
+//         'https://cartitem-ch19-default-rtdb.firebaseio.com/cart.json',
+//         {
+//           method: 'PUT',
+//           body: JSON.stringify(cart),
+//         }
+//       );
+
+//       if (!response.ok) {
+//         throw new Error('Sending cart data failed');
+//       }
+//       // const data = await response.json();
+
+//       dispatch(
+//         uiActions.showNotification({
+//           status: 'success',
+//           title: 'Success!',
+//           message: 'Sent cart data successfully!',
+//         })
+//       );
+//     };
+
+//     if (isInitial) {
+//       isInitial = false;
+//       return;
+//     }
+
+//     sendCartData().catch((error) => {
+//       dispatch(
+//         uiActions.showNotification({
+//           status: 'error',
+//           title: 'Error',
+//           message: 'Sending cart data failed!',
+//         })
+//       );
+//     });
+//   }, [cart, dispatch]);
+
+//   return (
+//     <Fragment>
+//       {notification && (
+//         <Notification
+//           status={notification.status}
+//           title={notification.title}
+//           message={notification.message}
+//         />
+//       )}
+//       <Layout>
+//         {showCart && <Cart />}
+//         <Products />
+//       </Layout>
+//     </Fragment>
+//   );
+// }
+
+// export default App;
